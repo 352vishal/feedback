33474c055d42892f9e6e600b32fe422f586205f6
352vishal <vishalhariyani5@gmail.com>
2024-05-28 21:31:48 +0530

    changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {Country} from '@angular-material-extensions/select-country'; 
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,8 @@ import { Component } from '@angular/core';
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'feedback';
+
+  onCountrySelected(country: Country) {
+    console.log(country);
+  }
 }
